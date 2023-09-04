917d38a91e933c3fb7b508f423ba61a864405a25
Daniel Hu <hdmdhr@gmail.com>
2023-09-04 10:19:23 +0900

    Initial Commit

---
+++ LocaleInDetail/LocaleInDetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleInDetailApp.swift
+//  LocaleInDetail
+//
+//  Created by Daniel Hu on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct LocaleInDetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
